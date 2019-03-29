6d9728da6da31aebd554c0e3169db11e44a4b536
guoyanfei <648064117@qq.com>
2019-03-29 18:06:43 +0800

    vue demo

---
+++ menu-demo/elt-slt-common/src/App.vue
@@ -0,0 +1,141 @@
+<template>
+  <div id="app">
+    <SLT :SLTData="SLTData"
+    v-on:listenLeftOneChange="leftOneChange"
+    v-on:listenSearchClick="searchClick"
+    v-on:listenTabsClick="tabsClick"
+    v-on:listenRightImgClick="rightImgClick"
+    v-on:listenTableRowClick="tableRowClick"
+    v-on:listenIconClick="iconClick"/>
+  </div>
+</template>
+
+<script>
+import RightData from './SLTConfig.js';
+import SLT from './components/Slt';
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      SLTData:{
+        //左侧第一个下拉列表数据
+        LeftoptionsOne: [{
+                value: '1',
+                label: '北京'
+                }, {
+                value: '2',
+                label: '上海'
+                }, {
+                value: '3',
+                label: '河北'
+            }],
+   
+        //右侧数据
+        RightData:RightData,
+        //左侧第二个下拉列表数据
+        LeftoptionsTwo: [],
+        //右侧表格的数据
+        rightTableData:[]
+      }
+      
+
+    }
+  },
+  components: {
+    SLT:SLT
+  },
+  methods: {
+    leftOneChange(LeftOnevalue){
+      //第一个下拉列表change事件,LeftOnevalue是选中的值
+       if(LeftOnevalue === "1"){
+            this.SLTData.LeftoptionsTwo=[
+                {value:'1',label:'朝阳'},
+                {value:'2',label:'海淀'},
+                {value:'3',label:'石景山'},
+                {value:'4',label:'丰台'}
+            ];
+        }else if(LeftOnevalue === "2"){
+            this.SLTData.LeftoptionsTwo=[
+                {value:'1',label:'黄埔路'},
+                {value:'2',label:'长春路'},
+                {value:'3',label:'天津路'}
+            ];
+        }else if(LeftOnevalue ==="3"){
+            this.SLTData.LeftoptionsTwo=[
+                {value:'1',label:'石家庄'},
+                {value:'2',label:'邯郸'},
+                {value:'3',label:'唐山'}
+            ];
+        }
+      
+    },
+    iconClick(item){
+        //监听右侧图片上的Icon点击事件
+        //paramItem.LeftOnevalue(左侧第一个默认下拉列表的值)
+        //paramItem.LeftTwovalue(左侧第二个默认下拉列表的值)
+        //paramItem.rightActiveIndex(右侧选中的tabs的index索引) 根据此索引可以查询信息
+        //paramItem.rightCardImgClickIndex(右侧选中的img的index索引) 根据此索引可以查询信息
+    },
+    searchClick(paramItem){
+        //左侧点击查询按钮事件
+        //paramItem.LeftOnevalue(左侧第一个默认下拉列表的值)
+        //paramItem.LeftTwovalue(左侧第二个默认下拉列表的值)
+        //paramItem.rightActiveIndex(右侧选中的tabs的index索引) 根据此索引可以查询信息
+        //paramItem.rightCardImgClickIndex(右侧选中的img的index索引) 根据此索引可以查询信息
+        this.SLTData.rightTableData=[
+            {
+                name:"张三",
+                age:"21",
+                sex:"男",
+                native:"北京"
+            },{
+                name:"李四",
+                age:"18",
+                sex:"男",
+                native:"上海"
+            },{
+                name:"赵武",
+                age:"23",
+                sex:"女",
+                native:"河南"
+            }
+        ]
+        
+    },
+    tabsClick(paramItem){
+        //右侧tabs点击事件
+        //paramItem.LeftOnevalue(左侧第一个默认下拉列表的值)
+        //paramItem.LeftTwovalue(左侧第二个默认下拉列表的值)
+        //paramItem.rightActiveIndex(右侧选中的tabs的index索引) 根据此索引可以查询信息
+        //paramItem.rightCardImgClickIndex(右侧选中的img的index索引) 根据此索引可以查询信息
+
+    },
+    rightImgClick(paramItem){
+        //右侧Img点击事件
+        //paramItem.LeftOnevalue(左侧第一个默认下拉列表的值)
+        //paramItem.LeftTwovalue(左侧第二个默认下拉列表的值)
+        //paramItem.rightActiveIndex(右侧选中的tabs的index索引) 根据此索引可以查询信息
+        //paramItem.rightCardImgClickIndex(右侧选中的img的index索引) 根据此索引可以查询信息
+
+    },
+    tableRowClick(row){
+        //右侧table行的点击事件
+        //row是返回当前点击的行的数据
+    }
+  },
+  mounted(){
+      
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ menu-demo/elt-slt-common/src/components/Slt.vue
@@ -0,0 +1,386 @@
+<template>
+    <div>
+        <el-row class="left-row">
+            <el-select size="small" v-model="paramItem.LeftOnevalue" placeholder="请选择" @change="leftOneChange()">
+                <el-option
+                v-for="item in SLTData.LeftoptionsOne"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+            <el-select size="small" v-model="paramItem.LeftTwovalue" placeholder="请选择" @change="leftTwoChange()">
+                <el-option
+                v-for="item in SLTData.LeftoptionsTwo"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+            <el-button type="primary" icon="el-icon-search" size="small" @click="leftButton"></el-button>
+        </el-row>
+
+        <el-row class="right-row">
+            <el-collapse v-model="collname" accordion>
+                <el-collapse-item name="1">
+                    <template slot="title">
+                        <div class="card-title">
+                            <i class="el-icon-location i-location"></i>
+                            <div>{{this.rightTile}}</div>
+                        </div>
+                    </template>
+                    <el-card class="box-card el-box-card">
+                        <div class="card-context" style="margin-top:-10px;">
+                            <el-tabs v-model="rightActiveName" type="border-card" @tab-click="righthandleClick">
+                                <el-tab-pane v-for="item in SLTData.RightData" :key="item.name" :label="item.title" :name="item.name">
+                                    <div style="overflow: hidden;">
+                                        <span class="card-span-icon-left" v-if="item.context.length >4" @click="cardspanleftevent(item.context.length)">
+                                            <i class="card-img-icon-left"></i>
+                                        </span>
+                                        <span class="card-span-icon-right" v-if="item.context.length >4" @click="cardspanrightevent(item.context.length)">
+                                            <i class="card-img-icon-right"></i>
+                                        </span>
+                                        <div class="card-tab-pane" :style="cardTabPane">
+                                            <div v-for="(itemimg,index) in item.context" :key="index" 
+                                            @mouseover="rightCardImgHoverIndex = index" 
+                                            @mouseleave="rightCardImgHoverIndex = -1" 
+                                            @click="rightCardImgClick(index,itemimg)"
+                                            :class="{'card-tab-pane-list':true,'card-tab-pane-list-over':index == rightCardImgHoverIndex,'card-tab-pane-list-selected':index == paramItem.rightCardImgClickIndex}">
+                                                <img :src="itemimg.imgsrc" class="card-tab-pane-list-img" >
+                                                <img src="../assets/sltimg/icon-selected.png" :class="{'card-tab-icon-img':true,'icon-show':!itemimg.iconShow}" @click.stop="rightCardIconClick(itemimg)" >
+                                                <span>{{itemimg.imgname}}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <el-table
+                                        :data="SLTData.rightTableData"
+                                        border
+                                        @row-click="tableRowClick"
+                                        height="332"
+                                        style="width: 100%; margin-top:5px;">
+                                        <el-table-column
+                                            v-for="(itemtablefield,fieldindex) in rightTableField"
+                                            :key="fieldindex"
+                                            :prop="itemtablefield.prop"
+                                            :label="itemtablefield.label"
+                                            :width="itemtablefield.width"
+                                            :header-align="itemtablefield.align"
+                                            style="background-color:#E4E7ED;">
+                                        </el-table-column>
+                                    </el-table>
+                                </el-tab-pane>
+                            </el-tabs>
+                        </div>
+                    </el-card>
+                </el-collapse-item>
+            </el-collapse>
+        </el-row>
+        
+    </div>
+</template>
+<script>
+import { 
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Row,
+  Card,
+  RadioGroup,
+  RadioButton,
+  Tabs,
+  TabPane,
+  Table,
+  TableColumn,
+  Collapse,
+  CollapseItem
+} from 'element-ui';
+export default {
+    name: "Slt",
+    props:{
+        SLTData:Object
+    },
+    data(){
+        return {
+            paramItem:{
+                //左侧第一个下拉列表值
+                LeftOnevalue:"",
+                //左侧第二个下拉列表值
+                LeftTwovalue:"",
+                //右侧当前tabs的index索引
+                rightActiveIndex:"0",
+                //右侧当前img的index索引
+                rightCardImgClickIndex:"0"
+            },
+            //右侧tabs的name值
+            rightActiveName:"",
+            rightTableField:[],
+            LeftOneLabel: '',
+            LeftTwoLabel: '',
+            rightTile:'',
+            rightCardImgHoverIndex:-1,
+            cardTabPane:'transform:translateX(0px)',
+            panevalue:0,
+            iconImgCount:4,
+            iconImgWidth:73,
+            collname:"1"
+        }
+    },
+    methods:{
+        leftOneChange(){//左侧数据绑定
+            let obj={};
+            obj =this.SLTData.LeftoptionsOne.find((item)=>{
+                return item.value === this.paramItem.LeftOnevalue;
+            });
+            this.LeftOneLabel = obj.label;
+            this.paramItem.LeftTwovalue = '';
+           
+            this.$emit("listenLeftOneChange",this.paramItem.LeftOnevalue);
+            this.paramItem.LeftTwovalue = this.SLTData.LeftoptionsTwo[0].value;
+            this.leftTwoChange();
+        },
+        leftTwoChange(){//左侧二级菜单改变事件
+            let obj={};
+            obj =this.SLTData.LeftoptionsTwo.find((item)=>{
+                return item.value === this.paramItem.LeftTwovalue;
+            });
+            this.LeftTwoLabel = obj.label;
+        },
+        leftButton(){//左侧按钮点击事件
+            this.rightTile=this.LeftOneLabel+"-"+this.LeftTwoLabel;
+            this.rightCardImgClick(this.paramItem.rightCardImgClickIndex,this.SLTData.RightData[this.paramItem.rightActiveIndex].context[this.paramItem.rightCardImgClickIndex]);
+            this.$emit("listenSearchClick",this.paramItem);
+        },
+        righthandleClick(tab,event){//右侧tabs点击事件
+            this.rightTableField =  [];
+            this.SLTData.rightTableData = [];
+            this.paramItem.rightActiveIndex = tab.index;
+            this.rightCardImgClick(0,this.SLTData.RightData[this.paramItem.rightActiveIndex].context[this.paramItem.rightCardImgClickIndex]);
+            this.$emit("listenTabsClick",this.paramItem);
+        },
+        rightCardImgClick(index,itemimg){//右侧图片点击事件
+            this.paramItem.rightCardImgClickIndex = index;
+            this.rightTableField =  itemimg.tableField;
+            itemimg.iconShow = true;
+            this.$emit("listenRightImgClick",this.paramItem);
+        },
+        rightCardIconClick(itemimg){//右侧点击Icon选中事件
+            itemimg.iconShow = !itemimg.iconShow;
+            this.$emit("listenIconClick",this.paramItem);
+        },
+        tableRowClick(row,event,column){//右侧表格数据点击事件
+            this.$emit("listenTableRowClick",row);
+        },
+        cardspanleftevent(itemcount){            
+
+            if(Math.floor(Math.abs(this.panevalue) / this.iconImgCount) > 0){
+                this.panevalue = this.panevalue + this.iconImgCount;
+            } else{
+                this.panevalue=0;
+            }
+            this.cardTabPane='transform:translateX('+(this.panevalue * this.iconImgWidth)+'px)';
+        },
+        cardspanrightevent(itemcount){
+            var xlength = itemcount - this.iconImgCount - Math.abs(this.panevalue);
+            if(Math.floor(xlength / this.iconImgCount) > 0){
+                this.panevalue=this.panevalue - this.iconImgCount;
+            }else{
+                this.panevalue=this.panevalue-xlength;
+            }
+            this.cardTabPane='transform:translateX('+(this.panevalue * this.iconImgWidth)+'px)';
+        }
+        
+    },
+    components: {
+        ElSelect: Select,
+        ElOption: Option,
+        ElButton: Button,
+        ElRow: Row,
+        ElCard: Card,
+        ElRadioGroup: RadioGroup,
+        ElRadioButton: RadioButton,
+        ElTabs: Tabs,
+        ElTabPane: TabPane,
+        ElTable: Table,
+        ElTableColumn: TableColumn,
+        ElCollapse: Collapse,
+        ElCollapseItem: CollapseItem
+    },
+    mounted(){
+        this.paramItem.LeftOnevalue=this.SLTData.LeftoptionsOne[0].value;
+        this.leftOneChange();
+        this.rightActiveName = this.SLTData.RightData[this.paramItem.rightActiveIndex].name;
+        this.leftButton();
+    }
+}
+</script>
+
+<style>
+    /* begin left */
+    .left-row{
+        top:28px; 
+        left:54px;
+        position:absolute;
+    }
+    .el-select {
+        width:180px;
+        float: left;
+        margin-right: 6px;
+    }
+    .el-button{
+        float: left;
+    }
+
+    /* end left */
+
+    /* begin right */
+    .right-row {
+        position: absolute;
+        top:90px;
+        right: 85px;
+        width:348px;
+        border:solid 1px #FFF;
+        border-radius: 4px;
+    }
+
+    .el-card__body{
+        padding:10px;
+    }
+    .i-location {
+        color:#66b1ff;
+        font-size: 18px;
+        margin-top:14px;
+    }
+    .card-title{
+        font-size: 16px;
+        display: flex;
+        margin-left: 8px;
+    }
+    .card-context{
+        margin-top:10px;
+    }
+    .card-tab-pane{
+        display:flex;
+        transition: transform .5s,-webkit-transform .5s;
+
+    }
+    .card-tab-pane-list{
+        float: left;
+        margin-right: 3px;
+        cursor: pointer;
+        margin-left: 4px;
+        width:66px;
+        height:98px;
+        
+    }
+    .card-tab-pane-list span{
+        font-size: 14px;
+        font-family:微软雅黑;
+        margin-left: 6px;
+    }
+    .card-tab-pane-list-over{
+        background: url(../assets/sltimg/ComBg02.png);
+    }
+    .card-tab-pane-list-selected{
+        background: url(../assets/sltimg/ComBg02.png);
+        
+    }
+    .card-tab-pane-list-img{
+        padding: 7px 7px 0px 7px;
+    }
+    .card-tab-icon-img{
+        width:28px;
+        height: 25px;
+        position: absolute;
+        margin-left: 32px;
+        top:34px;
+        z-index:10;
+    }
+    .icon-show{
+        display: none;
+    }
+    .el-tabs__item{
+        padding:0 10px;
+    }
+    .card-span-icon-left{
+        position:absolute;
+        width:8px;
+        height: 92px;
+        left:6px;
+        cursor: pointer;
+    }
+    .card-img-icon-left{
+         background:url(../assets/sltimg/select-icon-left.png);
+         width:8px;
+         height: 91px;
+         display: block;
+    }
+    .card-span-icon-right{
+        position: absolute;
+        width:8px;
+        height: 92px;
+        right: 6px;
+    }
+    .card-img-icon-right{
+        background:url(../assets/sltimg/select-icon-right.png);
+         width:8px;
+         height: 91px;
+         display: block;
+         cursor: pointer;
+    }
+    .el-tabs--border-card{
+        height: 540px;
+    }
+    .el-tabs--border-card{
+        height: 500px;
+    }
+    .el-table thead{
+        color: #409EFF;
+    }
+    .el-box-card{
+        border:0; 
+        box-shadow: 0 0px 0px 0 rgba(0,0,0,.1); 
+        -webkit-box-shadow: 0 0px 0px 0 rgba(0,0,0,.1);
+    }
+    .el-collapse-item__content{
+        padding-bottom:0px;
+    }
+    .el-collapse{
+        border-top: 0px solid #EBEEF5;
+        border-bottom: 0px solid #EBEEF5;
+    }
+    .has-gutter tr th{
+        background-color: #e3f2fb;
+    }
+    .hover-row{
+        cursor: pointer;
+    }
+    /* end right */
+
+    /*定义滚动条高宽及背景 高宽分别对应横竖滚动条的尺寸*/
+    ::-webkit-scrollbar
+    {
+        width: 12px;
+        height: 12px;
+        background-color: #F5F5F5;
+    }
+    
+    /*定义滚动条轨道 内阴影+圆角*/
+    ::-webkit-scrollbar-track
+    {
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.1);
+        border-radius: 5px;
+        background-color: #F5F5F5;
+    }
+    
+    /*定义滑块 内阴影+圆角*/
+    ::-webkit-scrollbar-thumb
+    {
+        border-radius: 5px;
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+        background-color: rgba(0,0,0,.2);
+    }
+
+</style>
+
