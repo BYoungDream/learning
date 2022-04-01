d50f8ed4afd67bbcbda523772042ba933a50dc1c
guoyanfei <body_guo@qq.com>
2022-04-01 16:38:57 +0800

    数据库生成文档

---
+++ spring-demo/databaseword/src/main/java/com/gyf/databaseword/DatabasewordApplication.java
@@ -0,0 +1,13 @@
+package com.gyf.databaseword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasewordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasewordApplication.class, args);
+    }
+
+}
